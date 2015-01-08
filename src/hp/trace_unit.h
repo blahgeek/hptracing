7cafc7ae0b6cd992d8c2832059ed274ccc6cf8d6
BlahGeek <blahgeek@gmail.com>
2015-01-08 12:45:40 +0800

    cleanup

--- src/hp/trace_unit.h
+++ src/hp/trace_unit.h
@@ -91,55 +91,6 @@ namespace Unit {
 
 }
 
-class TraceUnit {
-public:
-    /**
-     * The ID of primary light
-     */
-    int orig_id = -1;
-    /**
-     * Trace depth. Start with 0 (primary light)
-     */
-    int depth = 0;
-
-    Color strength;
-
-    Vec start_p;
-    Vec in_dir;
-
-    Geometry * geometry = nullptr;
-    Material * material = nullptr;
-
-    Vec normal;
-    Vec intersect_p;
-
-    Vec reflection_dir;
-    Vec refraction_dir;
-
-    Color result = Vec(0, 0, 0);
-
-public:
-    /**
-     * Given a scene, find THE geometry which intersect with (start_p->in_dir)
-     * geometry, material, intersect_p and result will be computed 
-     * (geometry may remains nullptr)
-     * @return True if found
-     */
-    bool findGeometry(Scene * scene);
-    /**
-     * Compute intersection information base on geometry previously found
-     * normal, intersect_p, reflection_dir, refeaction_dir will be computed
-     */
-    void computeIntersection();
-
-    using unit_insert_f = std::function<void(TraceUnit)>;
-    /**
-     * Sample all sub-trace, put it into target
-     */
-    void sampleSubTrace(Scene * scene, unit_insert_f insert_f);
-
-};
-
 }
 
 #endif
